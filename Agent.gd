abb3f329e1ea1701fe06e950b3e37a679b7bb08c
doorgan <dorgandash@gmail.com>
2021-03-30 13:19:56 -0300

    save

--- Agent.gd
+++ Agent.gd
@@ -6,11 +6,11 @@ var interest = []
 var danger = []
 var directions = []
 
-func _ready():
-	interest.resize(12)
-	danger.resize(12)
+func _init():
 	directions.resize(12)
 	for i in range(12):
+		interest.append(0)
+		danger.append(0)
 		var angle = (i * 2 * PI) / 12
 		directions[i] = Vector2.RIGHT.rotated(angle)
 		
@@ -28,12 +28,54 @@ func bh_chase():
 		var interest_values = []
 		for interest in interests:
 			var distance = position.distance_to(interest.position)
-			var dir = position - interest.position
+			var dir = interest.position - position
 			var dot = direction.normalized().dot(dir.normalized())
-			interest_values.append(dot)
+			interest_values.append(distance * dot)
 		var sum = 0
-		for value in interest_values:
+		
+		var values = _normalize(interest_values)
+		
+		for value in values:
 			sum += value
 		
-		interest[i] = sum
+		interest[i] = sum/values.size()
 	return interest
+
+
+func _max(list):
+	var ret
+	for val in list:
+		if not ret:
+			ret = val
+		else:
+			if val > ret:
+				ret = val
+	return ret
+
+func _min(list):
+	var ret
+	for val in list:
+		if not ret:
+			ret = val
+		else:
+			if val < ret:
+				ret = val
+	return ret
+	
+func _normalize(list):
+	print("=normalize=")
+	print(list)
+	var maxim = _max(list)
+	var minim = _min(list)
+	
+	var average = (minim + maxim) / 2
+	var rang = (maxim - minim) / 2
+	
+	var ret = []
+	
+	for x in list:
+		var val = (x - average) / rang
+		ret.append(val)
+	print(ret)
+	print("=end normalize=")
+	return ret
--- InterestGizmo.gd
+++ InterestGizmo.gd
@@ -1,8 +1,6 @@
-tool
 extends Node2D
 
 export var radius: float = 50
-export var interest_segments: int = 8
 
 func _process(_delta):
 	update()
@@ -13,17 +11,18 @@ func _draw():
 	draw_interest_segments()
 
 func draw_interest_segments():
+	var interest = get_parent().interest
 	var target = get_viewport().get_mouse_position() - global_position
-	var angle_step = 360.0 / interest_segments
+	var angle_step = 360.0 / interest.size()
 	
-	for i in range(interest_segments + 1):
+	for i in interest.size():
 		var angle_point = deg2rad(i * angle_step)
 		var angle_vector = Vector2(cos(angle_point), sin(angle_point))
 		var segment_start = angle_vector * radius
-		var segment_length = max(0, radius/2 * angle_vector.normalized().dot(target.normalized()))
+		var segment_length = max(0, radius/2 * interest[i])
 		var segment_end = angle_vector * (radius + segment_length)
 
-		var color = Color.green
+		var color = (Color.green if i > 1 else Color.red)
 		
 		draw_line(segment_start, segment_end, color)
 
